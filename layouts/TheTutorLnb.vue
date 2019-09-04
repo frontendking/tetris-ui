fe063909f93fdbd4b9158b3bfa83919f7b31b35b
ilhwan.ryu <ih.ryu.dev@gmail.com>
2019-09-04 17:35:36 +0900

    create pc layout

---
+++ layouts/TheTutorLnb.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="TheLnbB2c">
+    TheLnbB2c
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheLnbB2c'
+}
+</script>
+
+<style scoped lang=scss>
+.TheLnbB2c {
+}
+</style>
