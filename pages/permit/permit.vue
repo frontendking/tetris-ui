0c97cf18c29d8c4ad32ce907aeabdab80ae91609
ilhwan.ryu <ih.ryu.dev@gmail.com>
2019-09-16 14:01:50 +0900

    start

---
+++ pages/permit/permit.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="permit">
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'permit',
+  data() {
+    return {
+      nav: [
+        {
+
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang=scss>
+.permit {
+}
+</style>
