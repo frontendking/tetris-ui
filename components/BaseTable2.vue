4860579856e4abc889a1b1ff4211af9b596738c0
ryuilhwan <ryuilhwan@ryuilhwan-6.local>
2019-10-04 06:17:52 +0900

    create page having the table list

---
+++ components/BaseTable2.vue
@@ -0,0 +1,77 @@
+<template>
+  <table class="BaseTable2">
+    <caption v-if="captions">
+      <p v-for="caption in captions" :class="caption.id" v-html="caption.p"/>
+    </caption>
+
+    <thead>
+    <tr>
+      <th scope="col" v-for="v in colHeader" v-html="v.label"></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="(row, k) in tableData">
+      <td v-for="v in colHeader" v-html="row[v.id]"/>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'BaseTable2',
+  props: [
+    'captions',
+    'colHeader',
+    'tableData'
+  ],
+}
+</script>
+
+<style lang=scss>
+.BaseTable2 {
+  border-collapse: collapse;
+  border: 1px solid #000000;
+  color: #000;
+  table-layout: fixed;
+  width: 100%;
+  caption {
+    text-align: center;
+    font-size: 3rem;
+    font-weight: bold;
+    padding: 2rem 0;
+  }
+
+  td {
+    font-size: 1.2rem;
+    font-weight: normal;
+    border: 1px solid #b7b7b7;
+  }
+
+  th {
+    &[scope="col"]{
+      height: 3rem;
+    }
+    height: 3rem;
+    font-size: 1.6rem;
+    font-weight: 500;
+    color: #ffffff;
+    background-color: #6e737d;
+    text-align: center;
+    vertical-align: middle;
+
+    &:nth-child(1) {
+      min-width: 6rem;
+    }
+
+    &:nth-child(n+2) {
+    }
+
+    &[scope="row"] {
+      background-color: #f7f7f7;
+      color: #000000;
+      border: 1px solid #b7b7b7;
+    }
+  }
+}
+</style>
---
+++ pages/learning/review.vue
@@ -0,0 +1,103 @@
+<template>
+  <article class="review">
+    <h1 v-html="heading"/>
+    <BaseTable
+      id="attendance-table"
+      :colHeader = "colHeader"
+      :tableData = "tableData"
+    />
+
+  </article>
+</template>
+
+<script>
+import BaseTable from '../../components/BaseTable2'
+
+const colHeaderNames = [
+  '수강클래스', '1회차', '2회차', '3회차', '4회차', '후기등록률'
+]
+const seed = [
+  {
+    className: '[일간대치동]2기 튜터 실습',
+    lesson1: '수업일자 2018-10-22<button class="update">수정하기</button>',
+    lesson2: '수업일자 2018-10-22<button class="write">작성하기</button>',
+    lesson3: '수업일자 2018-10-22<button class="update">작성하기</button>',
+    lesson4: '수업일자 2018-10-22<button class="update">작성하기</button>',
+    reviewRatio: '25% (1/4)'
+  },
+]
+export default {
+  data() {
+    return {
+      heading: '리뷰작성하기',
+      searchForm: {
+        searchKeyword: {
+          legend: '검색어',
+          type: [
+            '전체',
+            '클래스명',
+            '선생님명'
+          ],
+          placeHolder: '검색어를 입력해 주세요'
+        },
+        searchBtn: '검색'
+      },
+      tableData:[
+        {
+          articleNo: '<span class="important">[중요한 공지사항]</span>',
+          title:'[수업 가이드] 7월 2,3,4주차 수업 가이드 안내',
+          author: 'tutor.com',
+          writeDate: '2019.07.24'
+        },
+        ...[...Array(10)].flatMap(()=>seed),
+      ],
+      colHeader: Object.keys(seed[0]).map((v,i)=>{
+        return {
+          id:v,
+          label: colHeaderNames[i]
+        }
+      })
+    }
+  },
+  components: {
+    BaseTable
+  }
+}
+</script>
+
+<style lang=scss>
+.review {
+  @media(min-width: 641px) {
+    display: grid;
+    /*@formatter:off*/
+    grid-template:
+      "header" auto
+      "table" auto/
+      auto;
+    /* @formatter:on*/
+    font-size: 1.6rem;
+    line-height: 1.56;
+    letter-spacing: normal;
+    color: #6e737d;
+
+    h1, h2 {
+      color: #000000;
+    }
+
+    > h1 {
+      grid-area: header;
+      display: initial;
+      font-size: 3.4rem;
+      font-weight: bolder;
+      line-height: 1;
+      padding-bottom: 1.8rem;
+      border-bottom: 2px solid #000000;
+    }
+
+    > #attendance-table {
+      margin-top: 3rem;
+    }
+  }
+}
+
+</style>
